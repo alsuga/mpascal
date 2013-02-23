4823b04e3f44f7218e4dd4396b4e3792a4ceab11
alsuga <ing.alsuga@gmail.com>
2013-02-22 20:01:53 -0500

    First commit

---
+++ errors.py
@@ -0,0 +1,101 @@
+# errors.py
+'''
+Soporte al manejo de errores del Compilador.
+
+Una de las partes mas importantes (y dificiles) de escribir un compilador
+es un reporte confiable de mensajes de error al usuario.  Este archivo
+define alguna funcionalidad generica para tratar con errores a traves
+del proyecto del compilador.  El manejo de errores se basa en un enfoque
+suscripcion/registro (subscription/logging based approach).
+
+Para reportar un error en el compilador, use la funcion error(). Por ejemplo:
+
+       error(lineno,"Alguna clase de mensaje de error del compilador")
+
+donde lineno es el numero de la linea en la cual ocurrio el error.  Si su
+compilador soporta multiples archivos fuentes, agrege el argumento clave filename.
+
+       error(lineno,"Alguna clase de mensaje de error del compilador",filename="foo.src")
+
+El manejo de errores es basado en el modelo base de suscripcion usando
+manejadores-contexto y la funcion subscribe_errors(). Por ejemplo, para enrutar
+el mensaje de error a la salida estandar, use esto:
+
+       with subscribe_errors(print):
+            run_compiler()
+
+Para enviar el mensaje al error estandar, usted puede hacer esto:
+
+       import sys
+       from functools import partial
+       with subscribe_errors(partial(print,file=sys.stderr)):
+            run_compiler()
+
+Para enrutar el mensaje a un registro, usted puede hacer esto:
+
+       import logging
+       log = logging.getLogger("somelogger")
+       with subscribe_errors(log.error):
+            run_compiler()
+
+Para recoger mensajes de error para el proposito de pruebas unitarias, haga esto:
+
+       errs = []
+       with subscribe_errors(errs.append):
+            run_compiler()
+       # Comprobar errs para errores especificos
+
+La funcíon de utilidad errors_reported() devuelve el numero total de
+errores reportados hasta el momento.  Las diferentes estapas del compilador
+podrian utilizar esto para decidir si se continua o no con el proceso.
+
+Utilice clear_errors() para borrar el numero total de errores.
+'''
+
+import sys
+from contextlib import contextmanager
+
+_subscribers = []
+_num_errors = 0
+
+def error(lineno, message, filename=None):
+	'''
+	Reporta un error del compilador a todos los suscriptores.
+	'''
+	global _num_errors
+	if not filename:
+		errmsg = "{}: {}".format(lineno, message)
+	else:
+		errmsg = "{}:{}: {}".format(filename,lineno,message)
+	for subscriber in _subscribers:
+		subscriber(errmsg)
+	_num_errors += 1
+
+def errors_reported():
+	'''
+	Devuelve el numero de errores reportados.
+	'''
+	return _num_errors
+
+def clear_errors():
+	'''
+	Limpia el numero total de errores reportados.
+	'''
+	global _num_errors
+	_num_errors = 0
+
+@contextmanager
+def subscribe_errors(handler):
+	'''
+	Gestor de contexto que permite el seguimiento de mensajes de error del compilador.
+	Utilicelo como sigue donde handler es un llamador tomando un solo argumento
+	el cual es la cadena de mensaje de error:
+
+	with subscribe_errors(handler):
+		... do compiler ops ...
+	'''
+	_subscribers.append(handler)
+	try:
+		yield
+	finally:
+		_subscribers.remove(handler)
---
+++ mpaslex.py
@@ -0,0 +1,391 @@
+#!/usr/bin/env python
+# mpaslex.py
+r'''
+Proyecto 1 - Escribir un Lexer
+==============================
+En este primer proyecto, se va a escribir un sencillo lexer para un
+lenguaje como mpascal.  El proyecto se presenta como codigo que se
+debe leer y completar (este archivo).  Por favor, lea el contenido
+completo de este archivo y cuidadosamente siga los pasos indicados
+por los comentarios.
+
+Resumen:
+--------
+El proceso del analisis lexico consiste en tomar una entrada y dividirla
+en un flujo de tokens (simbolos). Cada token es como una palabra valida
+del diccionario.  Esencialmente, la función del analizador lexico es
+simplemente asegurar que el texto de entrada se compone de simbolos validos
+antes de cualquier procesamiento adicional relacionado con el parsing 
+(analisis sintatico).
+
+Cada token esta definido por una expresion regular.  Por lo tanto, su
+tarea principal en este primer proyecto es definir un conjunto de expresiones
+regulares para el lenguaje.  El trabajo actual del analizador lexico es
+manejado por PLY (http://www.dabeaz.com/ply).
+
+
+Especificacion:
+---------------
+Su lexer debera reconocer los siguientes simbolos y tokens.  El nombre de la
+izquierda es el nombre del token, el valor de la derecha es el texto coincidente.
+
+Palabras Reservadas:
+    CONST   : 'const'
+    VAR     : 'var'  
+    PRINT   : 'print'
+    FUNC    : 'func'
+    EXTERN  : 'extern'
+
+Identificadores:  (Las mismas reglas que para Python)
+    ID      : Texto que comience con una letra o '_', seguido de cualquier numero
+              numero de letras, digitos o guion bajo.
+
+Operadores y Delimitadores:
+    PLUS    : '+'
+    MINUS   : '-'
+    TIMES   : '*'
+    DIVIDE  : '/'
+    ASSIGN  : '='
+    SEMI    : ';'
+    LPAREN  : '('
+    RPAREN  : ')'
+    COMMA   : ','
+
+Literales:
+    INTEGER : '123'   (decimal)
+              '0123'  (octal)
+              '0x123' (hex)
+
+    FLOAT   : '1.234'
+              '1.234e1'
+              '1.234e+1'
+              '1.234e-1'
+              '1e2'
+              '.1234'
+              '1234.'
+
+    STRING  : '"Hola Mundo\n"'
+
+Comentarios:  Deben ser ignorados por su analizador lexico
+     //             Salta el resto de la linea
+     /* ... */      Salta un bloque (anidacion no permitida)
+
+Errores: Su lexer debera reportar los siguientes mensajes de error:
+
+     lineno: Ilegal char 'c'
+     lineno: Cadena no terminada
+     lineno: Comentario sin terminar
+     lineno: Codigo de escape malo en cadena '\..'
+
+Pruebas
+-------
+Para el desarrollo inicial, intente ejecutar el lexer con un archivo de 
+entrada de ejemplo como:
+
+     bash % python mpaslex.py sample.pas
+
+Estudie cuidadosamente la salida del lexer y asegurese que tiene sentido.
+Una vez que este razonablemente satisfecho con el resultado, trate de 
+ejecutarlos con algunas pruebas mas dificiles:
+
+     bash % python mpaslex.py testlex1.pas
+     bash % python mpaslex.py testlex2.pas
+
+Bonos: ¿Que haria usted para convertir estas pruebas en pruebas unitarias
+adecuadas?
+'''
+
+# ----------------------------------------------------------------------
+# El siguiente import carga la funcion errror(lineno,msg) que debe ser
+# usada para reportar todos los mensajes de error emitidos por su lexer.
+# Pruebas unitarias y otras caracteristicas del compilador se apoyaran
+# en esta funcion.  Revise el archivo errors.py para mas documentacion
+# acerca del mecanismo del control de errores.
+from errors import error
+
+# ----------------------------------------------------------------------
+# Analizadores lexicos son definidos usando la libreria ply.lex
+#
+# Vea http://www.dabeaz.com/ply/ply.html#ply_nn3
+from ply.lex import lex
+
+# ----------------------------------------------------------------------
+# Lista de token. Esta lista identifica la lista completa de nombres de
+# token que deben ser reconocidos por su lexer.  No cambie ninguno de 
+# estos nombres. Haciendolo se rompera las pruebas unitarias.
+
+tokens = [
+	# keywords
+	'ID', 'CONST', 'VAR', 'PRINT', 'FUNC', 'EXTERN',
+
+	# Control flow
+	'IF', 'ELSE', 'WHILE',
+
+	# Operators and delimiters
+	'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
+	'ASSIGN', 'SEMI', 'LPAREN', 'RPAREN', 'COMMA',
+	'LBRACE', 'RBRACE',
+
+	# Boolean operators
+	'LT', 'LE', 'GT', 'GE', 'LAND', 'LOR', 'LNOT',
+	'EQ', 'NE',
+
+	# Literals
+	'INTEGER', 'FLOAT', 'STRING', 'BOOLEAN',
+]
+
+# ----------------------------------------------------------------------
+# Ignorando caracteres (whitespace)
+#
+# Los siguientes caracteres son ignorados completamente por el lexer.
+# No lo cambie.
+
+t_ignore = ' \t\r'
+
+# ----------------------------------------------------------------------
+# *** DEBE COMPLETAR : Escriba las regexpsindicadas a continuacion ***
+# 
+# Tokens para simbolos sencillos: + - * / = ( ) ;
+
+t_PLUS      = r'\+'
+t_MINUS     = r'-'
+t_TIMES     = r'\*'
+t_DIVIDE    = r'/'
+t_ASSIGN    = r'='
+t_SEMI      = r';'
+t_LPAREN    = r'\('
+t_RPAREN    = r'\)'
+t_LBRACE    = r'{'
+t_RBRACE    = r'}'
+t_COMMA     = r','
+t_LT        = r'<'
+t_LE        = r'<='
+t_EQ        = r'=='
+t_GE        = r'>='
+t_GT        = r'>'
+t_NE        = r'!='
+t_LAND      = r'&&'
+t_LOR       = r'\|\|'
+t_LNOT      = r'!'
+
+# ----------------------------------------------------------------------
+# *** DEBE COMPLETAR : escriba las regexps y codigo adicional para ***
+#
+# Tokens para literales, INTEGER, FLOAT, STRING. 
+
+# Constantes punto flotante.   Deben ser reconocidos numeros de punto
+# flotante en formatos como se muestra en los ejemplos siguientes:
+#
+#   1.23, 1.23e1, 1.23e+1, 1.23e-1, 123., .123, 1e1, 0.
+#
+# El valor debe ser convertido a un float de Python cuando se analice
+def t_FLOAT(t):
+	r'(?:\d*\.\d+|\d+\.\d*)(?:[eE][-+]?\d+)?'
+	t.value = float(t.value)               # Conversion a float de Python
+	return t
+
+# Constantes enteras.  Se debe reconocer enteros en todos los formatos
+# siguientes:
+#
+#     1234             (decimal)
+#     01234            (octal)
+#     0x1234 or 0X1234 (hex)
+#
+# El valor debe ser convertido a un int de Python cuando se analice.
+def t_INTEGER(t):
+	r'(?:0[xX]?)?\d+'
+	# Conversion a int de Python
+	if t.value.startswith(('0x','0X')):
+		t.value = int(t.value,16)              
+	elif t.value.startswith('0'):
+		t.value = int(t.value,8)
+	else:
+		t.value = int(t.value)
+	return t
+
+# Constantes string.  Se debe reconocer texto entre comillas.
+# Por ejemplo:
+#
+#     "Hola Mundo"
+#
+# A las cadenas se les permite tener codigos de escape los cuales
+# estan definidos por un backslash inicial.   Los siguientes 
+# codigos backslash deben ser reconocidos:
+#      
+#       \n    = newline (10)
+#       \r    = carriage return (13) 
+#       \t    = tab (9)
+#       \\    = char baskslash (\)
+#       \"    = comilla (")
+#       \bhh  = codigo caracter byte hh  (h es un digito hex)
+#
+# Todas los otros codigos de escape deberan dar un error. Nota: las 
+# literales de string *NO* comprenden el rango completo de codigos de
+# caracter soportados por Python.
+#
+# El valor del token debe ser la cadena con todos los codigo de escape
+# reemplazados por su correspondiente codigo de caracter raw.
+
+escapes_not_b = r'nrt\"'
+escapes = escapes_not_b + "b"
+def _replace_escape_codes(t):
+	r'''
+	*** SE DEBE IMPLEMENTAR ***
+
+	Reemplace todos los codigos de escape validos \.. en una cadena con
+	su codigo de caracter raw equivalente.
+	'''
+	newval = []
+	ostring = iter(t.value)
+	olen = len(t.value)
+	for c in ostring:
+		if c=='"':
+			error("Fin de cadena prematuro")
+		elif c=='\\':
+			c1 = ostring.next()
+			#if c1 not in escapes_not_b:
+			if c1 not in escapes:
+				error(t.lexer.lineno,"Codigo secuencia escape mala '%s'" % escape_code)
+			else:
+				if c1=='n':
+					c='\n'
+				elif c1=='r':
+					c='\r'
+				elif c1=='t':
+					c='\t'
+				elif c1=='\\':
+					c='\\'
+				elif c1=='"':
+					c='"'
+		newval.append(c)
+	else:
+		t.value = ''.join(newval)
+
+def t_STRING(t):
+	r'".*"'
+	# Convierta t.value a una cadena con codigo de escape reemplazado por valor actual.
+	t.value = t.value[1:-1]
+	_replace_escape_codes(t)    # Debe implementar arriba
+	return t
+
+def t_BOOLEAN(t):
+	r'\b(true|false)\b'
+	t.value = True if t.value=='true' else False
+	return t
+
+# ----------------------------------------------------------------------
+# *** DEBE COMPLETAR: Escrina la regexp y agrege la palabra ***
+#
+
+# Identificadores y palabras reservadas.
+# Coincidir un identificador raw.  Los identificadores siguen las mismas
+# reglas de Python. Esto es, ellos comienzan con una letra o subrayado (_)
+# y puede contener un numero arbitrario de letras, digitos o subrayado 
+# despues de este.
+def t_ID(t):
+	r'[a-zA-Z_]\w*'
+	t.type = reserved.get(t.value,'ID')
+	return t
+
+# *** DEBE IMPLEMENTAR ***
+# Adicione codigo para coincidir con palabras reservadas como 'var', 'const',
+# 'print', 'func', 'extern'
+
+reserved = {
+	'if':'IF',
+	'else':'ELSE',
+	'while':'WHILE',
+	'var':'VAR',
+	'const':'CONST',
+	'func':'FUNC',
+	'extern':'EXTERN',
+	'print':'PRINT',
+}
+
+operators = {
+	r'+' : "PLUS",
+	r'-' : "MINUS",
+	r'*' : "TIMES",
+	r'/' : "DIVIDE",
+	r'=' : "ASSIGN",
+	r';' : "SEMI",
+	r'(' : "LPAREN",
+	r')' : "RPAREN",
+	r',' : "COMMA",
+	r'<' : "LT",
+	r'<=' : "LE",
+	r'==' : "EQ",
+	r'>=' : "GE",
+	r'>' : "GT",
+	r'!=' : "NE",
+	r'&&' : "LAND",
+	r'||' : "LOR",
+	r'!' : "LNOT",
+}
+
+# ----------------------------------------------------------------------
+# *** DEBE COMPLETAR : Escriba las regexps adecuadas ***
+#
+# Ignore texto.  Las siguientes reglas debeb ser usadas para ignorar
+# texto en el archivo de entrada.  Esto incluye comentarios y lineas
+# en blanco.
+
+# Una o mas lineas en blanco
+def t_newline(t):
+	r'\n+'
+	t.lexer.lineno += len(t.value)
+
+# Comentarios C-style (/* ... */)
+def t_COMMENT(t):
+	r'/\*(.|\n)*?\*/'
+	t.lexer.lineno += t.value.count('\n')
+
+# Comentarios C++-style (//...)
+def t_CPPCOMMENT(t):
+	r'//.*\n'
+	t.lexer.lineno += 1
+
+# ----------------------------------------------------------------------
+# *** DEBE COMPLETAR : Agrege las regexps indicadas ***
+#
+# Manejo de error.  Las condiciones siguientes de error deben ser 
+# manejadas por su analizador lexico.
+
+# Caracteres ilegales (manejo errores generico)
+def t_error(t):
+	error(t.lexer.lineno,"Caracter ilegal %r" % t.value[0])
+	t.lexer.skip(1)
+
+# Comentario C-style no terminado
+def t_COMMENT_UNTERM(t):
+	r'/\*[.\n]*(?!\*/)'
+	error(t.lexer.lineno,"Comentario no terminado")
+
+# Literal de cadena no terminada
+def t_STRING_UNTERM(t):
+	r'"["]+'
+	error(t.lexer.lineno,"Literal de cadena no terminada")
+	t.lexer.lineno += 1
+
+# ----------------------------------------------------------------------
+#                NO CAMBIE NADA DEBAJO DE ESTA PARTE
+# ----------------------------------------------------------------------
+def make_lexer():
+	'''
+	Funcion de utilidad para crear el objeto lexer
+	'''
+	return lex()
+
+if __name__ == '__main__':
+	import sys
+	from errors import subscribe_errors
+
+	if len(sys.argv) != 2:
+		sys.stderr.write("Usage: %s filename\n" % sys.argv[0])
+		raise SystemExit(1)
+
+	lexer = make_lexer()
+	with subscribe_errors(lambda msg: sys.stderr.write(msg+"\n")):
+		lexer.input(open(sys.argv[1]).read())
+		for tok in iter(lexer.token,None):
+			sys.stdout.write("%s\n" % tok)
